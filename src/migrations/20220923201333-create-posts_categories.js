49dc63f65603dbbec97e0232d3698e25a279035d
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-23 17:34:42 -0300

    feat: criando migration para "posts_categories"

---
+++ src/migrations/20220923201333-create-posts_categories.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('post_categories', {
+      post_id: {
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+     await queryInterface.dropTable('users');  
+  }
+};
