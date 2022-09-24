c3b9a4c887cc2a81c636cfa491cb60cb837c526b
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-23 22:34:05 -0300

    feat: criando função para gerar token JWT

---
+++ src/utils/JWT.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
+
+const generateToken = async ({ id, email, password }) => {
+    const payload = {
+        id,
+        email,
+        password,
+    };
+    
+    const jwtConfig = {
+        expiresIn: '1d',
+        algorithm: 'HS256',
+    };
+
+    const token = jwt.sign(payload, secret, jwtConfig);
+    
+    return token;
+};
+
+module.exports = {
+    generateToken,
+};
