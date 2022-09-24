5a70f3d3811c648ea5c5c62ca25b7f56a966c8d1
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-23 22:17:56 -0300

    fix: removendo comentários

--- src/models/User.js
+++ src/models/User.js
@@ -11,21 +11,3 @@ const UserModel = (sequelize, DataTypes) => {
   };
   
   module.exports = UserModel;
-
-/*Se você usa MacOS
-Esse requisito pode dar um falso positivo! Garanta que o nome do arquivo está em PascalCase. O avaliador, que roda em Linux, é case-sensitive para arquivos, enquanto o MacOS, entre outros sistemas, são case-insensitive. Ou seja: na sua máquina pode rodar, e no avaliador não, então fique de olho! Caso queria se aprofundar nesse assunto, veja o seguinte link.
-
-Os seguintes pontos serão avaliados:
-[Será validado que existe o arquivo 'User.js']
-
-[Será validado que o modelo possui o nome 'User']
-
-[Será validado que o modelo possui a propriedade 'id']
-
-[Será validado que o modelo possui a propriedade 'display_name']
-
-[Será validado que o modelo possui a propriedade 'email']
-
-[Será validado que o modelo possui a propriedade 'password']
-
-[Será validado que o modelo possui a propriedade 'image'] */
