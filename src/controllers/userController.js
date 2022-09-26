64f32f70bfce3b4b0249f5f3d92b5f63aac875e2
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-26 17:22:54 -0300

    feat: criando controller para criar novo usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,6 +9,18 @@ const getAllUsers = async (req, res, next) => {
    }
 };
 
+const createNewUser = async (req, res, next) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+    console.log(displayName);
+    const newUser = await userService.createUser(displayName, email, password, image);
+    return res.status(200).json(newUser); 
+  } catch (error) {
+   next(error);
+  }
+};
+
 module.exports = {
     getAllUsers,
+    createNewUser,
 };
