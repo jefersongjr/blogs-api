38d169c2db6cc843c8717ba3ac0557c5d3979a72
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-26 18:35:25 -0300

    fix : excluindo comentários

--- src/services/userService.js
+++ src/services/userService.js
@@ -32,14 +32,3 @@ module.exports = {
     getAll,
     createUser,
 };
-
-/* O endpoint deve ser acessível através do URL /user;
-O endpoint deve ser capaz de adicionar um novo user a sua tabela no banco de dados;
-O corpo da requisição deverá seguir o formato abaixo:
-{
-  "displayName": "Brett Wiltshire",
-  "email": "brett@email.com",
-  "password": "123456",
-  "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
-  // a imagem não é obrigatória
-} */
