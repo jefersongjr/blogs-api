8d4bdf7beebe4a201a33f62178126d55403e2282
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-26 17:22:39 -0300

    feat: criando service para criar um novo usuario

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,6 +8,24 @@ const getAll = async () => {
    return users;
 };
 
+const createUser = async (displayName, email, password, image) => {
+    const newUser = await User.create({ displayName, email, password, image });
+
+    return newUser;
+};
+
 module.exports = {
     getAll,
+    createUser,
 };
+
+/* O endpoint deve ser acessível através do URL /user;
+O endpoint deve ser capaz de adicionar um novo user a sua tabela no banco de dados;
+O corpo da requisição deverá seguir o formato abaixo:
+{
+  "displayName": "Brett Wiltshire",
+  "email": "brett@email.com",
+  "password": "123456",
+  "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
+  // a imagem não é obrigatória
+} */
