5e64eb08034078b06bd21b7d335e4bb3f288ef6b
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-26 19:24:17 -0300

    feat : criando rota 'user/:id'

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const { getUserById } = require('../controllers/userController');
  const authMiddleware = require('../middlewares/authMiddleware');
 
 const authRoute = express.Router();
 
 authRoute.get('/user', authMiddleware, userController.getAllUsers);
+authRoute.get('/user/:id', authMiddleware, getUserById);
 authRoute.post('/user', userController.createNewUser);
 
 module.exports = authRoute;
