78836eef5e755c53d22ddc702923babd112040bd
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-26 18:32:21 -0300

    fix: adicionando filtros para cria novo usuario

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,28 @@
+const userMiddleware = require('../middlewares/userMiddleware');
 const { User } = require('../models');
 
 const getAll = async () => {
    const users = await User.findAll({
     attributes: ['id', 'displayName', 'email', 'image'],
 });
-   console.log(users);
    return users;
 };
 
 const createUser = async (displayName, email, password, image) => {
+    const { error } = userMiddleware.userSchema.validate({ displayName, email, password, image });
+     const test = await getAll();
+     const validateEmail = test.some((x) => x.email === email);
+     if (validateEmail) {
+      const status = 409;
+      const message = 'User already registered';
+      const erroMessage = { status, message };
+      throw erroMessage;
+     }
+      if (error) {
+    const [code, message] = error.message.split('|');
+    const x = { status: code, message };
+      throw x;
+  }
     const newUser = await User.create({ displayName, email, password, image });
 
     return newUser;
