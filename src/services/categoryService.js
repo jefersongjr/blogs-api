d09343540f0dbf9757b7c5ea8663a9044c9bde47
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-27 00:29:26 -0300

    feat: criando service para criar nova categoria

---
+++ src/services/categoryService.js
@@ -0,0 +1,19 @@
+const { Category } = require('../models');
+
+console.log(Category);
+
+const createCategory = async (name) => {
+    if (!name) {
+     console.log(name);
+     const status = 400;
+     const message = '"name" is required';
+     const erroMessage = { status, message };
+     throw erroMessage;
+    }
+    const category = Category.create({ name });
+    return category;
+};
+
+module.exports = { 
+ createCategory,
+};
