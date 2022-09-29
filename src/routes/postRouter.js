4b9254b5f0afc9b14acde931e189913c9042257a
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-28 22:45:27 -0300

    feat: criando rota post/:id

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -6,5 +6,6 @@ const postRoute = express.Router();
 
 postRoute.post('/post', authMiddleware, postController.createNewPost);
 postRoute.get('/post', authMiddleware, postController.getAllPost);
+postRoute.get('/post/:id', authMiddleware, postController.getPostById);
 
 module.exports = postRoute;
